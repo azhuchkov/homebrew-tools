31bc829748e5d3446d2588a51d09e1ab6b1b2037
Andrey Zhuchkov <azhuchkov@users.noreply.github.com>
2023-11-11 12:20:51 +0400

    Fixed tunblkctl installation

--- Formula/tunblkctl.rb
+++ Formula/tunblkctl.rb
@@ -1,13 +1,14 @@
 class Tunblkctl < Formula
   desc "Command-line frontend for Tunnelblick"
   homepage "https://github.com/azhuchkov/tunblkctl"
-  url "https://github.com/azhuchkov/tunblkctl/archive/refs/heads/main.zip"
+  url "https://github.com/azhuchkov/tunblkctl/archive/refs/tags/v0.1.0.alpha.tar.gz"
   version "0.1.0.alpha"
-  sha256 "3d530df2e6394869954ba225db84475eeb66be5d45498ccaa382119a9d5e2e2c"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   license "MIT"
 
   def install
+    prefix.install "tunblkctl-0.1.0.alpha/LICENSE", "tunblkctl-0.1.0.alpha/README.md"
     bin.mkpath
-    bin.install "tunblkctl"
+    bin.install "tunblkctl-0.1.0.alpha/tunblkctl"
   end
 end
