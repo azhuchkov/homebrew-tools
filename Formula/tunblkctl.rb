c9c5b13412570403658e16c5067ccc197561437f
Andrey Zhuchkov <azhuchkov@users.noreply.github.com>
2023-11-11 12:29:15 +0400

    Fixed tunblkctl hashsum

--- Formula/tunblkctl.rb
+++ Formula/tunblkctl.rb
@@ -3,7 +3,7 @@ class Tunblkctl < Formula
   homepage "https://github.com/azhuchkov/tunblkctl"
   url "https://github.com/azhuchkov/tunblkctl/archive/refs/tags/v0.1.0.alpha.tar.gz"
   version "0.1.0.alpha"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "b7e9582cb8f68092025fb31de89e326a63702620bf36e8034923c2067ce53336"
   license "MIT"
 
   def install
